d710dfe786b398d68190c5a43b3d4ff31a86af50
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2019-05-06 06:59:38 +0200

    day 4 code challenge update

--- Rust/minigrep/src/main.rs
+++ Rust/minigrep/src/main.rs
@@ -1,24 +1,67 @@
 use std::env;
-use std::fs;
+use std::process;
+
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+   let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    // let (query, filename) = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the fiie");
 
-    println!("With text:\n{}", contents);
+   if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+    // let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the fiie");
+
+    // println!("With text:\n{}", contents);
 
 }
 
+// all moved to lib file 
 
-fn parse_config(args: &[String]) -> (&str, &str){
-    let query = &args[1];
-    let flename = &args[2];
+// fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//     let contents = fs::read_to_string(config.filename)?;
 
-    (query, flename)
-}
+//     println!("With text:\n{}", contents);
+
+//     Ok(())
+// }
+
+
+
+// fn parse_config(args: &[String]) -> (&str, &str){
+//     let query = &args[1];
+//     let flename = &args[2];
+
+//     Config {query, filename}
+// }
+
+// impl Config {
+//     fn new(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
+
+//         let query = args[1].clone();
+//         let filename = args[2].clone();
+
+//         Ok(Config { query, filename })
+//     }
+// }
+
+// struct Config {
+//     query: String,
+//     filename: String,
+// }
